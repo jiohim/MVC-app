bd6857cb1ca35aed026bc17e4ac849bd164bcc73
Jiohim <bohdankrasin@gmail.com>
2022-09-04 16:58:08 +0200

    new version

--- src/main/java/pl/jiohim/springapp/controllers/FirstController.java
+++ src/main/java/pl/jiohim/springapp/controllers/FirstController.java
@@ -2,15 +2,25 @@ package pl.jiohim.springapp.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String HelloPage(){
+    public String HelloPage(@RequestParam(value = "name" , required = false) String name,
+                            @RequestParam(value ="surname", required = false) String surname,
+                            Model model){
+
+      //  System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
